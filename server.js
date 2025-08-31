5748e12e32f8a1ffac5b1fc0969008e501d3cb99
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-31 16:58:35 +0200

    Improve error message in error handling middleware

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Internal Server Error');
 });
 
 // Connect to MongoDB
