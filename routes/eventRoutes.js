b585ea06c37d79cb938fba824aaa9d5a1c468791
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-31 16:56:48 +0200

    feat: initial commit for routes/eventRoutes.js

---
+++ routes/eventRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// Create an event
+router.post('/', async (req, res) => {
+  try {
+    const newEvent = new Event(req.body);
+    const savedEvent = await newEvent.save();
+    res.status(201).json(savedEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Get all events
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update an event
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete an event
+router.delete('/:id', async (req, res) => {
+  try {
+    await Event.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Event deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
