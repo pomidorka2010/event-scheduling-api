1d8453aa623306380e4702f99b539a51c08c889c
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-31 16:56:46 +0200

    feat: initial commit for models/Event.js

---
+++ models/Event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  location: { type: String, required: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
